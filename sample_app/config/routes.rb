26159c55573812dc3f2490b25e3118bb2f2a2754
ハヤシ　カツヒロ <hayashikatsuhiro@hayashikatsuhironoiMac.local>
2021-11-11 19:17:29 +0900

    FinishCH11

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  root 'static_pages#home'
+  root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
@@ -9,4 +8,5 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   resources :users
+  resources :account_activations, only: [:edit]
 end
